acadf4497694ea152fa751352c08f39d8274b727
1sxyzn <soyun9902@ewhain.net>
2023-03-21 10:39:38 +0900

    Feat: Add Product Entity

---
+++ src/main/java/store/unibly/web/product/Product.java
@@ -0,0 +1,57 @@
+package store.unibly.web.product;
+
+import lombok.Builder;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import org.hibernate.annotations.ColumnDefault;
+import store.unibly.web.market.Market;
+
+import javax.persistence.*;
+import java.time.LocalDateTime;
+
+@NoArgsConstructor
+@Getter
+@Entity
+public class Product {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Integer id;
+
+    @ManyToOne
+    private Market market;
+
+    @Column(length = 50, unique = true)
+    private String name;
+
+    private Integer price;
+
+    @ColumnDefault("0")
+    private Integer sale;
+
+    private String mainImg; // 이미지 경로 저장
+
+    private String detailImg; // 이미지 경로 저장
+
+    @Column(columnDefinition = "TEXT")
+    private String detailDesc;
+
+    @Enumerated(EnumType.STRING)
+    private ProductCategory category;
+
+    private LocalDateTime createDate;
+
+    @Builder
+    public Product(Market market, String name, Integer price, Integer sale,
+                   String mainImg, String detailImg, String detailDesc,
+                   ProductCategory category, LocalDateTime createDate){
+        this.market=market;
+        this.name=name;
+        this.price=price;
+        this.sale=sale;
+        this.mainImg=mainImg;
+        this.detailImg=detailImg;
+        this.detailDesc=detailDesc;
+        this.category=category;
+        this.createDate=createDate;
+    }
+}
---
+++ src/main/java/store/unibly/web/product/ProductCategory.java
@@ -0,0 +1,18 @@
+package store.unibly.web.product;
+
+public enum ProductCategory {
+    Outer,
+    Top,
+    Set,
+    Pants,
+    Skirt,
+    Training,
+    Bag,
+    Shoes,
+    Accessory,
+    Homeware,
+    Jewelry,
+    Underwear,
+    Bigsize,
+    Beachwear
+}
